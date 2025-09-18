7a3fc4e7c601419883d17a7d96bbe16ecbe61954
hector1128 <hectorhoraciocordero11@gmail.com>
2025-09-18 02:52:26 -0400

    created some standard files + updated hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,20 +11,20 @@ export default function Home() {
         <p>PROJECTS</p>
       </div>
 
-      <div className="grid grid-cols-2 outline-black m-10 border-black">
+      <div className="grid grid-cols-2 m-10 border border-black outline-offset-20">
         <div>
-          <p>Hector Cordero</p>
+          <p className="text-6xl pb-8">Hector Cordero</p>
           <p className="pb-20">
-            On a journey to make the world a better place...
+            On a journey to make the <b>world</b> a better place...
           </p>
         </div>
         <div>
           <Image
-            src="/home/hero-image.jpg"
+            src="/home/hectorimg.jpg"
             alt="Hector Cordero"
             width={400}
             height={400}
-            className="rounded-full"
+            className="square-full "
           />
         </div>
       </div>
