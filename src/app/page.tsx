5af27c30adcc6aa6b345fd91b9f65cd24df2acc0
hector1128 <hectorhoraciocordero11@gmail.com>
2025-10-28 12:21:12 -0400

    updated resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -134,7 +134,7 @@ export default function Home() {
             />
           </Link>
           <Link
-            href="/home/Resume2025.pdf"
+            href="/home/hector_cordero_resume.pdf"
             className="transition-transform duration-200 hover:scale-110 hover:drop-shadow-[0_0_8px_white]"
           >
             <Image
